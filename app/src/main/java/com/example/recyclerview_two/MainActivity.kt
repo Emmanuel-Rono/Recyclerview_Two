6fe5b3d365038b5893f54300a56cdd255295a408
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2022-05-11 16:07:23 +0300

    Finishing up Recyclerview

--- app/src/main/java/com/example/recyclerview_two/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview_two/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.recyclerview_two
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dataTitle :Array<String> =resources.getStringArray(R.array.dataTitle)
+        val imageUrl:Array<String> =resources.getStringArray(R.array.imageUrl)
+        val gridLayout=GridLayoutManager(this,2)
+        val adapter=myAdapter(dataTitle, imageUrl)
+        val Recy=findViewById<RecyclerView>(R.id.Recy)
+        Recy.layoutManager=gridLayout
+        Recy.adapter=adapter
+
+
     }
 }
+
--- app/src/main/java/com/example/recyclerview_two/myAdapter.kt
+++ app/src/main/java/com/example/recyclerview_two/myAdapter.kt
@@ -1,24 +1,20 @@
 package com.example.recyclerview_two
 
-import android.icu.text.CaseMap
-import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
-import org.w3c.dom.Text
 
-class myAdapter(val dataTitles:Array<String>,val imageUrl:Array<String>)
+class myAdapter(val dataTitle:Array<String>,val imageUrl:Array<String>)
     :RecyclerView.Adapter<myAdapter.myViewHolder>() {
-//myAdapter receives data as declared in its constructor from the source where it is stored(Dataset)
+//myAdapter receives data as declared in its constructor from the source where it is
+// stored(Dataset)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myViewHolder {
 //we get the item views here to be used in our view
-
         val  view=LayoutInflater.from(parent.context).inflate(R.layout.griditem_view,parent,
             false)
         return myViewHolder(view)
@@ -26,15 +22,20 @@ class myAdapter(val dataTitles:Array<String>,val imageUrl:Array<String>)
 
 
     override fun onBindViewHolder(holder: myViewHolder, position: Int) {
-holder.TitleText.text=dataTitles(position)
+        //To access the data
+holder.TitleText.text=dataTitle[position]
         Picasso.get().load(imageUrl[position] ).into(holder.image)
     }
 
     override fun getItemCount(): Int {
-        return dataTitles.size
+        return dataTitle.size
     }
 
+
+
     class myViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //we will place data to this views
+        //bindView allocates data to this views
         val image=itemView.findViewById<ImageView>(R.id.Image)
         val TitleText=itemView.findViewById<TextView>(R.id.TitleText)
     }
